50e760aa8e2977cea79502f8fc3123b82ac44dd4
PedroCristino2020 <p.cristino.2020@alumnos.urjc.es>
2024-08-04 23:41:23 +0200

    diseño de pagina principal

---
+++ cosmic-news/src/test/java/es/codeurjc/cosmic_news/CosmicNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.cosmic_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmicNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
