0405eef815fcc7c8e1afc998279da3af9ceac8a0
PedroCristino2020 <p.cristino.2020@alumnos.urjc.es>
2024-11-18 18:18:27 +0100

    cambio de las mayusculas por minusculas

--- docker/create-image.sh
+++ docker/create-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login
-docker build -t PedroCristino2020/cosmic_news -f docker/Dockerfile .
-docker push PedroCristino2020/cosmic_news
+docker build -t pedrocristino2020/cosmic_news -f docker/Dockerfile .
+docker push pedrocristino2020/cosmic_news
