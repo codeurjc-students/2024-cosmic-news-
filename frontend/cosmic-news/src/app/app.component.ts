93fee94d22dbeda3a2beca5827bb54281cf271c4
PedroCristino2020 <p.cristino.2020@alumnos.urjc.es>
2024-12-19 23:52:06 +0100

    corregidos errores de imagenes en frontend de docker

--- frontend/cosmic-news/src/app/app.component.ts
+++ frontend/cosmic-news/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cosmic-news';
+  constructor(private router: Router) {
+    const isFront = window.location.pathname.includes('front');
+    const backgroundUrl = isFront ? '/images/background.jpg' : '/api/images/background';
+    document.documentElement.style.setProperty('--background-url', `url(${backgroundUrl})`);
+  }
 }
